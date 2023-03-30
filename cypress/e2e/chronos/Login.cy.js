e3532d7345d9fcc13f64a73f76ff39483a7e784e
Rwan Souza <rwan.souza@qwasolucoes.com.br>
2023-03-30 18:44:47 -0300

    finalizando a suite

--- cypress/e2e/chronos/Login.cy.js
+++ cypress/e2e/chronos/Login.cy.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 
-describe('Testes tela de login', () => {
+describe('Tela de Login', () => {
     beforeEach(() => {
       cy.visit('http://chronos.hml.qwa.brasil/login');
     })
 
-    it.skip('Testes de inputs', () => {
+    it('Testes de inputs', () => {
       cy.get('.login-form').should('have.attr', 'placeholder', 'Usuário').should('have.css', 'border-color', 'rgb(202, 214, 226)');
       cy.get('.input-content > .input-password').should('have.attr', 'placeholder', 'Senha').should('have.css', 'border-color', 'rgb(202, 214, 226)');
 
@@ -20,23 +20,22 @@ describe('Testes tela de login', () => {
       
     });
 
-    it.skip('Testes do botão', () => {
+    it('Testes do botão', () => {
       cy.get('.login-form').should('have.value', '');
       cy.get('.input-content > .input-password').should('have.value', '');
 
       cy.get('[type="submit"]').should('be.disabled');
       cy.get('[type="submit"]').contains('Enviar');
-      cy.get('.get-anchor').should('have.css', 'font-family', 'Roboto').should('have.css', 'font-size', '13px' );
     });
 
   
-    it.skip('Fazer login no chronos', () => {
+    it('Fazer login no chronos', () => {
 
       cy.get('.login-form').type('cypress.user');
       cy.get('.input-content > .input-password').type('admin');
   
       cy.intercept('POST', 'http://qwalx-02.qwa.brasil/chronos-oauth/oauth/token').as('login');
-      cy.get('app-submit-button.submit-button > .submit-button').click(); 
+      cy.get('[type="submit"]').click(); 
     
   
       cy.wait('@login').then((xhr) => {
@@ -44,7 +43,7 @@ describe('Testes tela de login', () => {
         expect(xhr.response.statusCode).be.eq(200);
       });
       
-      cy.url().should('be.equal', 'http://chronos.hml.qwa.brasil/dashboard')
+      cy.url().should('be.equal', 'http://chronos.hml.qwa.brasil/dashboard');
       
     });
 
@@ -58,4 +57,8 @@ describe('Testes tela de login', () => {
       
       cy.get('.col-10').contains('Usuário ou senha incorreto!');
     })
+
+    it('Esqueci minha senha', () => {
+      cy.get('.get-anchor').should('have.css', 'font-family', 'Roboto').should('have.css', 'font-size', '13px' );
+    });
   }); 
