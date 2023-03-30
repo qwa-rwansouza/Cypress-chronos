5344b71297560b9faf49d77e2ed2f86d20b1c431
Rwan Souza <rwan.souza@hotmail.com>
2023-03-30 18:12:06 -0300

    Organizando suites de testes

--- cypress/e2e/chronos/Login.cy.js
+++ cypress/e2e/chronos/Login.cy.js
@@ -5,7 +5,7 @@ describe('Testes tela de login', () => {
       cy.visit('http://chronos.hml.qwa.brasil/login');
     })
 
-    it('Testes de inputs', () => {
+    it.skip('Testes de inputs', () => {
       cy.get('.login-form').should('have.attr', 'placeholder', 'Usuário').should('have.css', 'border-color', 'rgb(202, 214, 226)');
       cy.get('.input-content > .input-password').should('have.attr', 'placeholder', 'Senha').should('have.css', 'border-color', 'rgb(202, 214, 226)');
 
@@ -20,48 +20,42 @@ describe('Testes tela de login', () => {
       
     });
 
-    it('Testes do botão', () => {
+    it.skip('Testes do botão', () => {
       cy.get('.login-form').should('have.value', '');
       cy.get('.input-content > .input-password').should('have.value', '');
 
       cy.get('[type="submit"]').should('be.disabled');
       cy.get('[type="submit"]').contains('Enviar');
+      cy.get('.get-anchor').should('have.css', 'font-family', 'Roboto').should('have.css', 'font-size', '13px' );
     });
 
   
-    it('Fazer login no chronos', () => {
+    it.skip('Fazer login no chronos', () => {
 
-      // cy.get('.login-form').should('have.attr', 'placeholder', 'Usuário');
-      // cy.get('.login-form').type('cypress.souza');
-      // cy.get('.input-content').type('admin');
-
-
-      cy.get('.login-form').should('have.css', 'border-color', 'rgb(202, 214, 226)');
-
-      cy.get('.login-form').type('x').clear();
-      cy.get('.input-content > .input-password').type('admin').clear();
-      
-      cy.get('.logo-chronos').click();
-
-      cy.get('.login-form').should('have.css', 'border-color', 'rgb(244, 92, 78)');
-      cy.get('.input-content > .input-password').should('have.css', 'border-color', 'rgb(244, 92, 78)');
-
-
-      cy.get('.get-anchor').should('have.css', 'font-family', 'Roboto').should('have.css', 'font-size', '13px' ); // yields 'sans-serif'
-
-
-      
-      // cy.get('app-submit-button.submit-button > .submit-button').click(); 
-      // cy.get('.toast-body')
-      // cy.get('.col-10').contains('Usuário ou senha incorreto!');
+      cy.get('.login-form').type('cypress.user');
+      cy.get('.input-content > .input-password').type('admin');
   
-      // cy.intercept('POST', 'http://www.chronos.qwasolucoes.com.br/chronos-oauth/oauth/token').as('login');
-      // cy.get('app-submit-button.submit-button > .submit-button').click(); 
+      cy.intercept('POST', 'http://qwalx-02.qwa.brasil/chronos-oauth/oauth/token').as('login');
+      cy.get('app-submit-button.submit-button > .submit-button').click(); 
     
   
-      // cy.wait('@login').then((xhr) => {
-      //   console.log(xhr)
-      //   expect(xhr.response.statusCode).be.eq(200);
-      // });    
+      cy.wait('@login').then((xhr) => {
+        console.log(xhr)
+        expect(xhr.response.statusCode).be.eq(200);
+      });
+      
+      cy.url().should('be.equal', 'http://chronos.hml.qwa.brasil/dashboard')
+      
     });
+
+    it('Testes toasty', () => {
+      cy.get('.login-form').type('cypress.user');
+      cy.get('.input-content > .input-password').type('admiaan');
+      cy.get('[type="submit"]').click();
+      
+      cy.get('.login-form').clear();
+      cy.get('.toast-body')
+      
+      cy.get('.col-10').contains('Usuário ou senha incorreto!');
+    })
   }); 
